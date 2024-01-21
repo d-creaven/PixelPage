414db8ad079bc3417b1cfddae622def699269d2a
Daragh Creaven <d.creaven7@universityofgalway.ie>
2024-01-21 22:19:41 +0000

    Added config for firestore

--- PixelPage/FireBaseConfig.ts
+++ PixelPage/FireBaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth}  from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
