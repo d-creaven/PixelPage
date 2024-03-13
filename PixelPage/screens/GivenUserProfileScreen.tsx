fda622887aba77036d709de4e9004d3e3e7fd6da
Daragh Creaven <144898567+d-creaven@users.noreply.github.com>
2024-03-13 21:17:38 +0000

    review feed on given user screen

--- PixelPage/screens/GivenUserProfileScreen.tsx
+++ PixelPage/screens/GivenUserProfileScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity, Pressable, FlatList } from 'react-native';
 import { auth, db } from '../FirebaseConfig';
 import { arrayRemove, arrayUnion, collection, doc, increment, onSnapshot, query, where, writeBatch } from 'firebase/firestore';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
+import ReviewItem from '../components/ReviewItem';
 
 const GivenUserProfileScreen = ({ route, navigation }) => {
   const [userData, setUserData] = useState({
@@ -17,25 +18,9 @@ const GivenUserProfileScreen = ({ route, navigation }) => {
     profileImageUrl: 'https://via.placeholder.com/150',
   });
 
+  const [userReviews, setUserReviews] = useState([]);
+  const [following, setFollowing] = useState(false);
   const { userId } = route.params;
-
-  useEffect(() => {
-    // Setup the reviews count listener
-    if (userId) {
-      const reviewsRef = collection(db, 'reviews');
-      const reviewsQuery = query(reviewsRef, where('userId', '==', userId));
-
-      const unsubscribeReviews = onSnapshot(reviewsQuery, (querySnapshot) => {
-        const userReviewsCount = querySnapshot.size;
-        setUserData((prevUserData) => ({
-          ...prevUserData,
-          reviewsCount: userReviewsCount, // Update the reviews count
-        }));
-      });
-
-      return () => unsubscribeReviews();
-    }
-  }, [userId]);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(doc(db, 'users', userId), (document) => {
@@ -44,7 +29,6 @@ const GivenUserProfileScreen = ({ route, navigation }) => {
         setUserData((prevUserData) => ({
           ...prevUserData,
           username: data.username || 'Loading...',
-          // Don't overwrite reviewsCount here since it's managed by a separate useEffect
           followers: data.followers || [],
           following: data.following || [],
           followersCount: data.followersCount || 0,
@@ -52,45 +36,44 @@ const GivenUserProfileScreen = ({ route, navigation }) => {
           bio: data.bio || '',
           profileImageUrl: data.profileImageUrl || 'https://via.placeholder.com/150',
         }));
-  
+
         navigation.setOptions({ headerTitle: data.username || 'Profile' });
         const isFollowing = data.followers?.includes(auth.currentUser?.uid);
         setFollowing(isFollowing);
-      } else {
-        // Handle the case where the document does not exist
       }
-    }, (error) => {
-      console.error("Error fetching user data: ", error);
     });
-  
+
     return () => unsubscribe();
   }, [userId, navigation]);
 
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerLeft: () => (
-        <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 10 }}>
-          <Ionicons name="arrow-back" size={24} color="black" />
-        </TouchableOpacity>
-      ),
-    });
-  }, [navigation]);
+  useEffect(() => {
+    if (userId) {
+      const reviewsRef = collection(db, 'reviews');
+      const reviewsQuery = query(reviewsRef, where('userId', '==', userId));
 
-  const [following, setFollowing] = useState(false);
+      const unsubscribeReviews = onSnapshot(reviewsQuery, (querySnapshot) => {
+        const reviews = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setUserReviews(reviews);
+        setUserData((prevUserData) => ({
+          ...prevUserData,
+          reviewsCount: reviews.length,
+        }));
+      });
+
+      return () => unsubscribeReviews();
+    }
+  }, [userId]);
 
   const handleFollowPress = async () => {
-    const currentUserUid = auth.currentUser?.uid; // UID of the logged-in user
-    const profileUserUid = userId; // UID of the user being viewed
-  
-    // References to both users' documents in Firestore
+    const currentUserUid = auth.currentUser?.uid;
+    const profileUserUid = userId;
+
     const currentUserRef = doc(db, 'users', currentUserUid);
     const profileUserRef = doc(db, 'users', profileUserUid);
-  
+
     try {
-      // Start a batch write
       const batch = writeBatch(db);
-  
-      // Check if already following, then unfollow
+
       if (following) {
         batch.update(currentUserRef, {
           following: arrayRemove(profileUserUid),
@@ -100,9 +83,8 @@ const GivenUserProfileScreen = ({ route, navigation }) => {
           followers: arrayRemove(currentUserUid),
           followersCount: increment(-1)
         });
-        setFollowing(false); // Update following state to false
+        setFollowing(false);
       } else {
-        // If not already following, follow
         batch.update(currentUserRef, {
           following: arrayUnion(profileUserUid),
           followingCount: increment(1)
@@ -111,39 +93,63 @@ const GivenUserProfileScreen = ({ route, navigation }) => {
           followers: arrayUnion(currentUserUid),
           followersCount: increment(1)
         });
-        setFollowing(true); // Update following state to true
+        setFollowing(true);
       }
-  
-      // Commit the batch write
+
       await batch.commit();
     } catch (error) {
       console.error("Error updating follow status: ", error);
     }
   };
-  
+
+  const renderReviewItem = ({ item }) => (
+    <View style={styles.reviewItem}>
+      <Text>{item.title}</Text>
+      {/* Render additional review details here */}
+    </View>
+  );
+
+  const renderHeader = () => (
+    <View style={styles.headerContainer}>
+      <Image source={{ uri: userData.profileImageUrl }} style={styles.profileImage} />
+      <Text style={styles.username}>{userData.username}</Text>
+      <View style={styles.statsContainer}>
+        <Text style={styles.stat}>{userData.reviewsCount} Reviews</Text>
+        <Text style={styles.stat}>{userData.followersCount} Followers</Text>
+        <Text style={styles.stat}>{userData.followingCount} Following</Text>
+      </View>
+      <Text style={styles.bio}>{userData.bio}</Text>
+      <Pressable
+        style={[styles.button, following ? { backgroundColor: 'lightgray' } : { backgroundColor: '#47AA71' }]}
+        onPress={handleFollowPress}
+      >
+        <Text style={styles.buttonText}>{following ? 'Following' : 'Follow'}</Text>
+      </Pressable>
+    </View>
+  );
 
   return (
-    <SafeAreaView edges={["top"]} style={styles.container}>
-      <ScrollView style={styles.container}>
-        <View style={styles.headerContainer}>
-          <Image source={{ uri: userData.profileImageUrl }} style={styles.profileImage} />
-          <View style={styles.statsContainer}>
-            <Text style={styles.stat}>{userData.reviewsCount} Reviews</Text>
-            <Text style={styles.stat}>{userData.followersCount} Followers</Text>
-            <Text style={styles.stat}>{userData.followingCount} Following</Text>
-          </View>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Image
+          source={{ uri: userData.profileImageUrl }} // Use profile image URL from state
+          style={styles.profileImage}
+        />
+        <Text style={styles.username}>{userData.username}</Text>
+        <View style={styles.statsContainer}>
+          <Text style={styles.stat}>{userData.reviewsCount} Reviews</Text>
+          <Text style={styles.stat}>{userData.followersCount} Followers</Text>
+          <Text style={styles.stat}>{userData.followingCount} Following</Text>
         </View>
         <Text style={styles.bio}>{userData.bio}</Text>
-        <Pressable
-          style={[styles.button, following ? { backgroundColor: 'lightgray' } : {backgroundColor: '#47AA71'}]}
-          onPress={(handleFollowPress)}
-        >
-          <Text style={styles.buttonText}>
-            {following ? 'Following' : 'Follow'}
-          </Text>
-        </Pressable>
+        {/* You can place any other static content here, like a follow button */}
+      </View>
+      <ScrollView style={styles.reviewsContainer}>
+        {userReviews.map((review) => (
+          <ReviewItem key={review.id} review={review} />
+        ))}
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
 
